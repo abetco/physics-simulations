a6bc8a01a35e8037b260a7f48150c2882e5e2b96
Albert Cao <albertc4@illinois.edu>
2018-11-15 14:30:48 -0600

    added press r to reset

--- finalproject-abetco/src/ofApp.cpp
+++ finalproject-abetco/src/ofApp.cpp
@@ -1,6 +1,4 @@
 #include "ofApp.h"
-//static int pts[] = { 257,219,257,258,259,274,263,325,266,345,266,352,269,369,276,387,286,415,291,425,302,451,308,462,316,472,321,480,328,488,333,495,339,501,345,505,350,507,365,515,370,519,377,522,382,525,388,527,405,534,426,538,439,539,452,539,468,540,485,540,496,541,607,541,618,539,625,537,641,530,666,513,682,500,710,476,723,463,727,457,729,453,732,450,734,447,738,440,746,423,756,404,772,363,779,343,781,339,784,327,789,301,792,278,794,267,794,257,795,250,795,232,796,222,796,197,797,195,797,188,796,188 };
-//static int nPts = 61 * 2;
 //--------------------------------------------------------------
 void ofApp::setup(){
 	box2d.init();
@@ -41,14 +39,10 @@ void ofApp::draw(){
 	// draw the ground
 	box2d.drawGround();
 
-	//string info = "";
-	//info += "Press [c] for circles\n";
-	//info += "Press [b] for blocks\n";
-	//info += "Total Bodies: " + ofToString(box2d.getBodyCount()) + "\n";
-	//info += "Total Joints: " + ofToString(box2d.getJointCount()) + "\n\n";
-	//info += "FPS: " + ofToString(ofGetFrameRate(), 1) + "\n";
-	//ofSetHexColor(0x444342);
-	//ofDrawBitmapString(info, 30, 30);
+	string info = "";
+	info += "Press [r] to reset!\n";
+	ofSetHexColor(0x444342);
+	ofDrawBitmapString(info, 10, 100);
 }
 
 //--------------------------------------------------------------
