08148ea689fc6acefb9f9ef03568aa50360c9f96
Albert Cao <albertc4@illinois.edu>
2018-11-30 01:07:34 -0600

    added javadoc comments for pendulum.h

--- finalproject-abetco/src/pendulum.h
+++ finalproject-abetco/src/pendulum.h
@@ -17,9 +17,27 @@ public:
 
 	/*float calculatePeriod();
 	float calculateTension();*/
+
+	/*Calculate the x position of the pendulum object based on the angle of the string with the vertical
+	@param angle: angle of the pendulum
+	@return the x position of the pendulum object*/
 	float calculateXPos(float angle);
+
+	/*Calculate the y position of the pendulum object based on the angle of the string with the vertical
+	@param angle: angle of the pendulum
+	@return the y position of the pendulum object*/
 	float calculateYPos(float angle);
+
+	/*Calculate the angle of the pendulum object based on the time elapsed
+	@param time: time passed since beginning of simulation or reset
+	@return the angle of the pendulum with the verticle
+	*/
 	float calculateAngle(float time);
+
+	/*Draws the string and circle representing the pendulum
+	@param xpos: X coordinate of the pendulum object
+	@param ypos: Y coordinate of the pendulum object
+	*/
 	void drawObj(float xpos, float ypos);
 	/*void drawTension(float xpos, float ypos);
 	void drawPeriod();*/
@@ -33,7 +51,7 @@ private:
 	ofParameter<float> length_, gravity_, angle_;
 	ofxPanel gui_;
 	ofParameterGroup parameters_;
-	ofxBox2d                                 box2d_;   // the box2d world
+	ofxBox2d box2d_;
 };
 
 #endif
