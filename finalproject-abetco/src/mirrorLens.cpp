a10a0329b60f975c2593cc2900bf92b5b8f9407b
Albert Cao <albertc4@illinois.edu>
2018-12-04 21:52:25 -0600

    created files for buoyancy and mirrorlens files for simulations

---
+++ finalproject-abetco/src/mirrorLens.cpp
@@ -0,0 +1,21 @@
+#include "mirrorLens.h"
+
+mirrorLens::mirrorLens() {
+}
+
+void mirrorLens::setup() {
+
+}
+
+void mirrorLens::update() {
+	
+}
+
+void mirrorLens::draw() {
+	
+}
+
+void mirrorLens::keyPressed(int key) {
+	
+	}
+}
---
+++ finalproject-abetco/src/mirrorLens.h
@@ -0,0 +1,25 @@
+#ifndef MIRROR_LENS_H
+#define MIRROR_LENS_H
+
+#include "ofMain.h"
+#include "ofxBox2d.h"
+#include "ofxGui.h"
+
+class twoBalls {
+public:
+	void setup();
+	void update();
+	void draw();
+	void keyPressed(int key);
+
+	twoBalls();
+
+private:
+	ofParameter<float> x_vel1_, y_vel1_, radius1_, x_vel2_, y_vel2_, radius2_;
+	ofxPanel gui_;
+	ofParameterGroup parameters_;
+	ofxBox2d                                 box2d_;
+	vector     <shared_ptr<ofxBox2dCircle> > circles_;
+};
+
+#endif
